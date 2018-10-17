325deb3454451c3c2cfa82c958c050efaa20d9fb
ecaoile <unfie555@gmail.com>
2018-10-17 12:12:20 -0700

    test page working

---
+++ JS_Ecommerce/JS_Ecommerce/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JS_Ecommerce
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Includes support for Razor Pages and controllers.
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+        }
+    }
+}
