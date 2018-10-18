0eb143d17731f8cfffeebb51315d2b10900c8acc
ecaoile <unfie555@gmail.com>
2018-10-17 17:46:02 -0700

    stopping point: added styling to promotions page

--- JS_Ecommerce/JS_Ecommerce/Models/Promo.cs
+++ JS_Ecommerce/JS_Ecommerce/Models/Promo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,11 +10,19 @@ namespace JS_Ecommerce.Models
     {
         public double OrderSubTotal { get; set; }
         public double PromoAmt { get; set; }
+
+        [Display(Name="ID")]
         public string PromoId { get; set; }
+
+        [Display(Name="Promo Name")]
         public string PromotionName { get; set; }
         public string Start { get; set; }
         public string End { get; set; }
+
+        [Display(Name="Minimum Order Value")]
         public double MinimumOrderValue { get; set; }
+
+        [Display(Name="Promotion Type")]
         public string PromotionType { get; set; }
     }
 }
--- JS_Ecommerce/JS_Ecommerce/Pages/Index.cshtml.cs
+++ JS_Ecommerce/JS_Ecommerce/Pages/Index.cshtml.cs
@@ -12,37 +12,9 @@ namespace JS_Ecommerce.Pages
 {
     public class IndexModel : PageModel
     {
-        public List<Promo> Promos { get; set; }
-        public async void OnGet()
+        public void OnGet()
         {
-            using (var client = new HttpClient())
-            {
-                try
-                {
-                    // add the appropriate properties on top of the client base address.
-                    client.BaseAddress = new Uri("http://jst.edchavez.com");
 
-                    //the .Result is important for us to extract the result of the response from the call
-                    var promosResponse = client.GetAsync("/api/promo/").Result;
-
-                    if (promosResponse.EnsureSuccessStatusCode().IsSuccessStatusCode)
-                    {
-                        var promosStringResult = await promosResponse.Content.ReadAsStringAsync();
-
-                        //TodoList datTodoList = JsonConvert.DeserializeObject<TodoList>(listStringResult);
-                        Promos = JsonConvert.DeserializeObject<List<Promo>>(promosStringResult);
-
-                        //List<TodoItem> matchedItems = demTodoItems.Where(i => i.DatListID == id).ToList();
-
-                        //datTodoList.TodoItems = matchedItems;
-                        //return View(datTodoList);
-                    }
-                }
-                catch
-                {
-                    //return NotFound();
-                }
-            }
         }
     }
 }
