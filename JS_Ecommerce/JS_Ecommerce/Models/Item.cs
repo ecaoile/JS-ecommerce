9cbc5686f5cff975d8edcd4f750e239e8003d41e
ecaoile <unfie555@gmail.com>
2018-10-18 20:25:02 -0700

    stopping point - got inventory to display properly

---
+++ JS_Ecommerce/JS_Ecommerce/Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JS_Ecommerce.Models
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        [Display(Name="Item ID")]
+        public string ItemId { get; set; }
+        [Display(Name="In Stock")]
+        public bool InStock { get; set; }
+    }
+}
