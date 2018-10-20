11d883249c7a0b5da829eb0be6cea60cb5281171
ecaoile <unfie555@gmail.com>
2018-10-19 17:57:07 -0700

    stopping point - moved inventory to home page

--- JS_Ecommerce/JS_Ecommerce/Pages/Index.cshtml.cs
+++ JS_Ecommerce/JS_Ecommerce/Pages/Index.cshtml.cs
@@ -7,14 +7,42 @@ using JS_Ecommerce.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace JS_Ecommerce.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
 
+        public List<Item> Inventory { get; set; }
+        public async Task OnGetAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    // add the appropriate properties on top of the client base address.
+                    client.BaseAddress = new Uri("http://jst.edchavez.com");
+
+                    //the .Result is important for us to extract the result of the response from the call
+                    var promosResponse = client.GetAsync("/api/inventory/getInventory/").Result;
+
+                    if (promosResponse.EnsureSuccessStatusCode().IsSuccessStatusCode)
+                    {
+                        var promosStringResult = await promosResponse.Content.ReadAsStringAsync();
+
+                        var inventoryArray = JsonConvert.DeserializeObject<ItemList>(promosStringResult);
+                        Inventory = inventoryArray.Items;
+                        //dynamic res = inventoryArray[0];
+                        //var inventoryArray = JsonConvert.DeserializeObject<List<ItemList>>(promosStringResult);
+                        //Inventory = inventoryArray[0];
+                    }
+                }
+                catch
+                {
+                    //return NotFound();
+                }
+            }
         }
     }
 }
